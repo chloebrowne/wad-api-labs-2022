0e35d580d41f0522e983ddc54bfc9f10ca861007
Chloe <20088355@mail.wit.ie>
2022-04-05 09:56:17 +0100

    Mongo lab Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import './db';
+import './seedData'
+
+
 
 dotenv.config();
 
@@ -8,7 +12,9 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
 app.use('/api/movies', moviesRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
